906fc97839a039f37d82aec32699c1ad4b50d555
Alexander Suslov <sasha_suslov07@mail.ru>
2022-08-13 04:18:49 +0300

    refactor: remove jquery from menu script

--- museum-dom/scripts/menu.js
+++ museum-dom/scripts/menu.js
@@ -1,4 +1,12 @@
+const headerNav = document.querySelector('.header-nav')
+const gamburger = document.querySelector('.gamburger');
 
-$(".gamburger").click(function(event) {
-  $(".gamburger, .header-nav").toggleClass("active");
+gamburger.addEventListener('click', () => {
+  if (gamburger.classList.contains('active')) {
+    gamburger.classList.remove('active');
+    headerNav.classList.remove('active');
+  } else {
+    gamburger.classList.add('active');
+    headerNav.classList.add('active');
+  }
 });
